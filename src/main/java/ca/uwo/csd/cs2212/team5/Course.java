a59309558309d3bf251c4b337c1b00df9e66d22c
Meg Starling <mstarlin@uwo.ca>
2014-02-28 09:00:56 -0500

    fix bug

--- src/main/java/ca/uwo/csd/cs2212/team5/Course.java
+++ src/main/java/ca/uwo/csd/cs2212/team5/Course.java
@@ -41,7 +41,7 @@ public void addStudent(Student newStudent){
 }
 
 public Student getStudent(String studentNumber){
-	Student found;
+	Student found=NULL;
 	Iterator<Student> iter=this.students.iterator();
 	while(iter.hasNext()){
 	   Student s=iter.next();
